843b4cafd3e28e761d01dd8e11fd2ff293b2fb8e
komosyu4649 <komosyu4649@gmail.com>
2020-04-12 11:34:44 +0900

    kanji

--- src/components/Top/Top.jsx
+++ src/components/Top/Top.jsx
@@ -8,7 +8,7 @@ function Top() {
         <div className="top">
           <div className="top-message">
            <div className="one">
-             <h2>にちじょうをたいせつに</h2>
+             <h2>日常をたいせつに</h2>
            </div>
            <div className="two">
              <h2>Shuhei&nbsp;Komori</h2>
