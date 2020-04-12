ca07975b6942a1475f61c1a93dbd7f96769d088e
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2020-04-12 12:15:35 +0900

    Updates

---
+++ precache-manifest.ae820a7985d0e85d950ac5f9e2be1f91.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf3c21c67a5f8f65cfca7fc8ede00550",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "ee445a832eb81af30bc8",
+    "url": "/test/static/css/main.0410a52f.chunk.css"
+  },
+  {
+    "revision": "48e574ebc96a70c400ef",
+    "url": "/test/static/js/2.e940f2ff.chunk.js"
+  },
+  {
+    "revision": "13cfc30d04b34d32b3959ef7194a4499",
+    "url": "/test/static/js/2.e940f2ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee445a832eb81af30bc8",
+    "url": "/test/static/js/main.c6eda663.chunk.js"
+  },
+  {
+    "revision": "d0d555fc18683b3d6055",
+    "url": "/test/static/js/runtime-main.c7d7daa2.js"
+  },
+  {
+    "revision": "1151dae4aa371586614523a54e3ed9d0",
+    "url": "/test/static/media/port-1.1151dae4.jpeg"
+  },
+  {
+    "revision": "5826834b4b838d9689e8d435de4a4d71",
+    "url": "/test/static/media/port-3.5826834b.jpg"
+  },
+  {
+    "revision": "8a95fe0df146f3026da55239eed31745",
+    "url": "/test/static/media/port-4.8a95fe0d.jpeg"
+  },
+  {
+    "revision": "21db1a5442a68dd0a6a50d76e2eb9a70",
+    "url": "/test/static/media/port-5.21db1a54.jpg"
+  },
+  {
+    "revision": "064fb897592c495d99f8d53e2f09b80b",
+    "url": "/test/static/media/port-6.064fb897.jpeg"
+  }
+]);
